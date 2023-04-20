d11a48f2051b3545162099caec50467b1374d9cd
ArangoHenaoSantiago <105956467+WumpusHd@users.noreply.github.com>
2023-04-19 21:27:45 -0500

    Add files via upload

---
+++ TAREA 5/Punto1-2/lista.cpp
@@ -0,0 +1,63 @@
+#include "lista.h"
+
+
+Lista::Lista()
+{
+	act = NULL;
+}
+
+
+void Lista::anxLista(int elemento){
+  Nodo *temporal, *primero;
+  Nodo* nuevo = new Nodo;
+  nuevo->dato = elemento;
+  nuevo->sig = nuevo;
+  nuevo->ant = nuevo;
+
+  if(act == NULL)
+    act = nuevo;
+  else{
+    temporal = act->ant;
+    temporal->sig = nuevo;
+    nuevo->ant = temporal;
+    nuevo->sig = act;
+    act->ant = nuevo;
+  }
+}
+
+int Lista::infoLista(int posicion){
+  Nodo* temporal = act;
+  for(int i = 1; i < posicion; i++)
+    temporal = temporal->sig;
+
+  return temporal->dato;
+}
+
+
+int Lista::longLista(){
+  int ans;
+  if(vaciaLista())
+    ans = 0;
+  else{
+    Nodo* temporal, *primero;
+    int contador = 1;
+    temporal = act;
+    primero = act;
+
+    while(temporal->sig != primero){
+      temporal = temporal->sig;
+      contador++;
+    }
+
+    ans = contador;
+  }
+
+  return ans;
+}
+
+bool Lista::vaciaLista()
+{
+	return act == NULL;
+}
+
+
---
+++ TAREA 5/Punto1-2/main.cpp
@@ -0,0 +1,80 @@
+#include "lista.h"
+
+int contarOcurrencias(Lista &l, int elemento)
+{
+	int elementoRepetido = 0;
+	int compararElemento;
+	if(l.vaciaLista())
+		elementoRepetido = 0; 
+	else
+	{
+		int i = 0;
+		while(i <= l.longLista())
+		{
+			compararElemento = l.infoLista(i);
+			if(elemento == compararElemento)
+				elementoRepetido++;
+			i++;
+		}
+	}
+	return elementoRepetido;
+}
+
+
+
+Lista obtenerMenores(Lista &l, int elemento)
+{
+	int compararElemento;
+	Lista l2;
+	if(l.vaciaLista())
+		printf("Lista vacia"); 
+	else
+	{
+		int i = 1;
+		while(i <= l.longLista())
+		{
+			compararElemento = l.infoLista(i);
+			if(compararElemento < elemento)
+			{
+				l2.anxLista(compararElemento);
+			}
+			i++;
+			
+		
+		}
+	}
+	return l2;
+		
+}
+
+
+
+
+
+
+int main()
+{
+	/* Implementacion Punto 1*/
+
+	Lista l1;
+	l1.anxLista(4);
+	l1.anxLista(12);
+	l1.anxLista(1);
+	l1.anxLista(7);
+	l1.anxLista(5);
+	l1.anxLista(3);
+	l1.anxLista(5);
+	l1.anxLista(9);
+	//int  elementoRepetido = contarOcurrencias(l1,5);
+	//printf("%d", elementoRepetido);
+
+
+	/*Implementacion punto 2 */
+	Lista l2 = obtenerMenores(l1,5);
+
+
+	for(int x = 1; x <= l2.longLista(); x++)
+			printf("[%d]", l2.infoLista(x));
+	return 0;
+
+}
