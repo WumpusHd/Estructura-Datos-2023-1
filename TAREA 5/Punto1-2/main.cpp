77b4075618fe767f88c42f2c6f9493890b5193f9
ArangoHenaoSantiago <105956467+WumpusHd@users.noreply.github.com>
2023-04-19 22:36:07 -0500

    Update main.cpp

--- TAREA 5/Punto1-2/main.cpp
+++ TAREA 5/Punto1-2/main.cpp
@@ -3,54 +3,64 @@
 
 
 /*
+
+Estudiante: Santiago Arango Henao
+
+Codigo: 8971445
+
+Tarea #5
+*/
+
+
+/*
 Complejidad Algoritmica ContarOcurrencias:
 
-La complejidad del algoritmo Contar Ocurrencias es de O(n^2) por la razón que si tenemos una lista de N elementos,
-entonces tomará N iteraciones para determinar cuantas veces se repite el elemento v, pero si dicho elemento v no existiera
-en la lista, tomaría N iteraciones para encontrarlo, por lo cual se tomaría que la complejidad seria O(n^2)*/
+La complejidad del algoritmo Contar Ocurrencias es de O(((3n)/2)) por la razón que si tenemos una lista de N elementos,
+entonces tomará N iteraciones para determinar cuantas veces se repite el elemento v, ya que la complejidad para calcular
+el largo de una lista circular doble es O(n), además tener en cuenta que se usa la operacion infoLista que tiene un costo
+de O(n/2), la cual se emplea para conocer el valor de cada posicion en la lista. Teniendo todo esto en cuenta para el peor caso,
+se concluye que la complejidad de la operacion ContarOcurrencias es O(((3n)/2)).
+*/
 
 int contarOcurrencias(Lista &l, int elemento)
 {
 	int elementoRepetido = 0;
 	int compararElemento;
-	if(l.vaciaLista())
-		elementoRepetido = 0; 
-	else
+	int i = 0;
+	while(i <= l.longLista())
 	{
-		int i = 0;
-		while(i <= l.longLista())
-		{
-			compararElemento = l.infoLista(i);
-			if(elemento == compararElemento)
-				elementoRepetido++;
-			i++;
-		}
+		compararElemento = l.infoLista(i);
+		if(elemento == compararElemento)
+			elementoRepetido++;
+		i++;
 	}
+	
 	return elementoRepetido;
 }
 
+/*Complejidad Algoritmica ObtenerMenor:
+
+Para calcular la complejidad del siguiente algoritmo, se tiene en cuenta el costo de cada una 
+de las operaciones usadas de listas circulares dobles, longLista O(n) , infoLista O(n/2) y anxLista O(1).
+En el peor caso, se tendría una lista de n elementos, dentro de los cuales se retornara una lista con aquellos
+numeros menores al elemento pasado como parametro. Sería recorrer una lista de n elementos, donde n elementos sean menores
+al elemento dado y finalmente agregarlos a una lista, sería una complejidad de O(n) + O(n/2) +O(1) = O(((3n)/2)+1), casi identica
+a la complejidad del anterior algoritmo 
 
 Lista obtenerMenores(Lista &l, int elemento)
 {
-	int compararElemento;
-	Lista l2;
-	if(l.vaciaLista())
-		printf("Lista vacia"); 
-	else
+	int i = 1;
+	while(i <= l.longLista())
 	{
-		int i = 1;
-		while(i <= l.longLista())
+		compararElemento = l.infoLista(i);
+		if(compararElemento < elemento)
 		{
-			compararElemento = l.infoLista(i);
-			if(compararElemento < elemento)
-			{
-				l2.anxLista(compararElemento);
-			}
-			i++;
-			
-		
+			l2.anxLista(compararElemento);
 		}
+		i++;
+
 	}
+
 	return l2;
 		
 }
