0901b58b7ee733466d7aa2dec95d856622d6769d
ArangoHenaoSantiago <105956467+WumpusHd@users.noreply.github.com>
2023-04-20 00:29:27 -0500

    Update main.cpp

--- TAREA 5/Punto3/B/main.cpp
+++ TAREA 5/Punto3/B/main.cpp
@@ -1,16 +1,32 @@
 #include "lista.h"
 
 int main(){
-  Lista l1;
-  l1.insListaOrdenada(4);
-  l1.insListaOrdenada(12);
-  l1.insListaOrdenada(1);
-  l1.insListaOrdenada(7);
-  l1.insListaOrdenada(14);
-  l1.insListaOrdenada(3);
-  l1.insListaOrdenada(8);
-  l1.insListaOrdenada(9);
-  
-  
+  Lista l;
+  l.insListaOrdenada(4);
+  l.insListaOrdenada(12);
+  l.insListaOrdenada(1);
+  l.insListaOrdenada(7);
+  l.insListaOrdenada(14);
+  l.insListaOrdenada(3);
+  l.insListaOrdenada(8);
+  l.insListaOrdenada(9);
+  /*printf("%d", l.infoLista(1));
+  printf("\n");
+  printf("%d", l.infoLista(2));
+  printf("\n");
+  printf("%d", l.infoLista(3));
+  printf("\n");
+  printf("%d", l.infoLista(4));
+  printf("\n");
+  printf("%d", l.infoLista(5));
+  printf("\n");
+  printf("%d", l.infoLista(6));
+  printf("\n");
+  printf("%d", l.infoLista(7));
+  printf("\n");
+  printf("%d", l.infoLista(8));
+  */
+
+
   return 0;
 }
