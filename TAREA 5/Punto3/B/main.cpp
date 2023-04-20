7943672fcb0ea156db25204302c26d810e56fb43
ArangoHenaoSantiago <105956467+WumpusHd@users.noreply.github.com>
2023-04-19 23:59:46 -0500

    Add files via upload

---
+++ TAREA 5/Punto3/B/main.cpp
@@ -0,0 +1,16 @@
+#include "lista.h"
+
+int main(){
+  Lista l1;
+  l1.insListaOrdenada(4);
+  l1.insListaOrdenada(12);
+  l1.insListaOrdenada(1);
+  l1.insListaOrdenada(7);
+  l1.insListaOrdenada(14);
+  l1.insListaOrdenada(3);
+  l1.insListaOrdenada(8);
+  l1.insListaOrdenada(9);
+  
+  
+  return 0;
+}
