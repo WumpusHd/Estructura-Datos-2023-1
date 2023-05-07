607371ffc7cf54375a202866f7e6e10e41b47c13
ArangoHenaoSantiago <105956467+WumpusHd@users.noreply.github.com>
2023-05-06 21:20:41 -0500

    Add files via upload

---
+++ proyecto/bigInteger.cpp
@@ -0,0 +1,65 @@
+/*
+ *
+ * Autor: Santiago Arango Henao
+ *Fecha Creacion: 30 de Abril 2023
+ *Fecha Ultima Modificacion: 
+ *Version 1
+ *
+ *Archivo implementacion Prototipos Big Integer
+ */
+
+
+
+#include "bigInteger.h"
+
+metodosBigInteger::constructorBigInteger(string entero){
+	Lista l;
+
+	for(int i = 0; i < entero.length()+1; ++i)
+	{
+		if(entero[i] == '0')
+			l.anxLista(0);
+
+		if(entero[i] == '1')
+			l.anxLista(1);
+
+		if(entero[i] == '2')
+			l.anxLista(2);
+
+		if(entero[i] == '3')
+			l.anxLista(3);
+
+		if(entero[i] == '4')
+			l.anxLista(4);
+
+		if(entero[i] == '5')
+			l.anxLista(5);
+
+		if(entero[i] == '6')
+			l.anxLista(6);
+
+		if(entero[i] == '7')
+			l.anxLista(7);
+
+		if(entero[i] == '8')
+			l.anxLista(8);
+
+		if(entero[i] == '9')
+			l.anxLista(9);
+	}
+
+	cout << "Valor Posicion 1: " << l.infoLista(1) << endl;
+	cout << "Valor Posicion 2: " << l.infoLista(2) << endl;
+	cout << "Valor Posicion 3: " << l.infoLista(3) << endl;
+	cout << "Valor Posicion 4: " << l.infoLista(4) << endl;
+
+}
+
+
+
+/*
+metodosBigInteger::constructorBigIntegerCopia()
+{
+	//int bigInteger2 = constructorBigInteger();
+}
+*/
---
+++ proyecto/bigInteger.h
@@ -0,0 +1,68 @@
+/*
+ *
+ * Autor: Santiago Arango Henao
+ *Fecha Creacion: 30 de Abril 2023
+ *Fecha Ultima Modificacion: 
+ *Version 1
+ *
+ *Archivo implementacion Prototipos Big Integer
+ */
+
+
+#ifndef __BIGINTEGER_H
+#define __BIGINTEGER_H
+
+#include "lista.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+class bigInteger{
+
+	private:
+		string bigInteger;
+		Lista l;
+};
+
+
+class metodosBigInteger{
+	public:
+
+		/*Operaciones del TAD BIG INTEGER */
+
+
+		/*      Constructoras       */
+		constructorBigInteger(string);
+
+		//constructorBigIntegerCopia();
+
+		/*      Modificadoras       */
+
+		void add(bigInteger);
+		void product(bigInteger);
+		void substract(bigInteger);
+		void quotient(bigInteger);
+		void remainder(bigInteger);
+		void pow(bigInteger);
+		metodosBigInteger operator +(bigInteger);
+		metodosBigInteger operator -(bigInteger);
+		metodosBigInteger operator *(bigInteger);
+		metodosBigInteger operator /(bigInteger);
+		metodosBigInteger operator %(bigInteger);
+		metodosBigInteger operator ==(bigInteger);
+		metodosBigInteger operator <(bigInteger);
+		metodosBigInteger operator <=(bigInteger);
+
+
+		Lista sumarListaValores(bigInteger);
+		Lista MultiplicarListaValores(bigInteger);
+
+		/*      Analizadoras      */
+		string toString();
+
+};
+
+#endif
+
