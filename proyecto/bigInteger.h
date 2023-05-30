d3304d8d44d4f030058a1ef315ddce89948b0eb5
ArangoHenaoSantiago <105956467+WumpusHd@users.noreply.github.com>
2023-05-30 07:52:22 -0500

    Update bigInteger.h

--- proyecto/bigInteger.h
+++ proyecto/bigInteger.h
@@ -1,3 +1,14 @@
+/*
+ *
+ * Autor: Santiago Arango Henao
+ *Fecha Creacion: 30 de Abril 2023
+ *Version 1
+ *
+ *Archivo implementacion Prototipos Big Integer
+ */
+
+
+
 #ifndef BIGINTEGER_H
 #define BIGINTEGER_H
 
@@ -11,10 +22,18 @@ private:
     std::vector<int> enterosGrandes;
 
 public:
+    /*Operaciones del TAD BIG INTEGER */
+
+
+
+    /*      Constructoras       */
+    
     BigInteger();
     BigInteger(const std::string& number);
     BigInteger(const BigInteger& entero);
 
+
+    /*      Modificadoras       */
 
     void add(BigInteger& entero);
 
@@ -29,8 +48,12 @@ public:
     void pow(int exponente);
 
 
+    /*      Analizadora      */
+
     std::string toString();
 
+
+    /*  Sobrecarga de Operadores */
 
     BigInteger operator+(BigInteger& entero);
 
@@ -41,6 +64,10 @@ public:
     BigInteger operator/(BigInteger& entero);
 
     BigInteger operator%(BigInteger& entero);
+
+
+
+    /*  Operadores Booleanos */ 
 
     bool operator==(BigInteger& entero);
 
